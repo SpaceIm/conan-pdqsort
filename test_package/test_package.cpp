713973753772b1b51b7e68360b23af6b9a4f7232
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-03 01:07:54 +0200

    C++99 example

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,15 +1,15 @@
 #include <pdqsort.h>
 
-#include <array>
 #include <iostream>
 
 int main() {
-    std::array<int, 10> s = {3, -102, 4, 30, 432, -13531, -43, 1, 0, -3};
+    int v[10] = {3, -102, 4, 30, 432, -13531, -43, 1, 0, -3};
 
-    pdqsort(s.begin(), s.end());
+    pdqsort(v, v + 10);
     for (int i = 0; i < 10; ++i) {
-        std::cout << s[i] << ' ';
+        std::cout << v[i] << ' ';
     }
     std::cout << std::endl;
+
     return 0;
 }
